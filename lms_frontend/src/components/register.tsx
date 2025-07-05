5912b00045785b7515d54a1ddb46b8c220f4c72a
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-07-05 12:57:36 +0100

    added dashboard to register

--- lms_frontend/src/components/register.tsx
+++ lms_frontend/src/components/register.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Navigaton from "./navigation"
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Register() {
@@ -81,7 +82,9 @@ function Register() {
   }, []);
 
   return (
-    <>
+    <div className ="registerBody">
+    <Navigaton />
+    <div className="register">
       <div className="top">
         <div>
           <h4>G.G able medical laboratory</h4>
@@ -247,7 +250,8 @@ function Register() {
       </div>
 
       }
-    </>
+      </div>
+    </div>
   );
 }
 
