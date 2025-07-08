c513f2b39cde46ad2b93259346bbb17c954d9871
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-07-08 10:42:00 +0100

    edit parameters successfully done

--- lms_frontend/src/components/serviceRender.tsx
+++ lms_frontend/src/components/serviceRender.tsx
@@ -6,7 +6,13 @@ import Navigation from "./navigation";
 function RenderServiceDetail(props: { serviceId: number }) {
   const navigate = useNavigate();
   const [service, setService] = useState<any>(null);
-
+  const [edit, setEdit] = useState(false)
+  const [name, setName] = useState("ee")
+  const [unit, setUnit] = useState(" ")
+  const [referenceValue, setReferenceValue] = useState("")
+  const [parameterId, setParameterId] = useState<number>()
+  const [serviceId, setServiceId] =useState<number>()
+  
   const getServiceDetails = async (serviceId: number) => {
     try {
       const response = await axios.get(`http://localhost:5000/service/${serviceId}`);
@@ -16,6 +22,21 @@ function RenderServiceDetail(props: { serviceId: number }) {
       console.error("Error fetching service:", error);
     }
   };
+
+
+  const editParameter = (parameterId : number, serviceId : number, name : string, unit : string, referenceValue : string )=> {
+    setEdit(true)
+    try {
+      setName(name)
+      setUnit(unit)
+      setReferenceValue(referenceValue)
+      setParameterId(parameterId)
+      setServiceId(serviceId)
+
+    }catch(error) {
+      console.error
+    }
+  }
 
   const returnBack = () => {
     navigate("/services");
@@ -30,8 +51,8 @@ function RenderServiceDetail(props: { serviceId: number }) {
       <Navigation />
       <div className="serviceDetail">
         <div className="heading">
-          <button onClick={returnBack}>
-            <i className="bi bi-arrow-left-short"></i> Back
+          <button className="back" onClick={returnBack}>
+            <i className="bi bi-arrow-left-short">back</i>
           </button>
           <h2>Service Name: {service ? service.name : "..."}</h2>
           <h3>Price: {service ? service.price : "..."} </h3>
@@ -50,6 +71,8 @@ function RenderServiceDetail(props: { serviceId: number }) {
           </h3>
         </div>
 
+{
+  !edit &&
         <div className="testParameter">
           <div className="eachParamsHeading">
             <div><p>Name</p></div>
@@ -60,7 +83,9 @@ function RenderServiceDetail(props: { serviceId: number }) {
             <div></div>
           </div>
 
-          {service?.testParameters?.map((parameter: any, index: number) => (
+          {
+          
+          service?.testParameters?.map((parameter: any, index: number) => (
             <div className="eachParams" key={index}>
               <p>{parameter.name}</p>
               <p>{parameter.unit}</p>
@@ -75,10 +100,43 @@ function RenderServiceDetail(props: { serviceId: number }) {
                   ? parameter.updatedAt.split("T")[0]
                   : parameter.updatedAt}
               </p>
+              <div>
+                <button className="editButton" onClick={()=> {editParameter(parameter.id,parameter.serviceTemplateId, parameter.name, parameter.unit, parameter.referenceValue)}}><i className="bi bi-pencil-fill"></i></button>
+                <button className="deleteButton"><i className="bi bi-trash3-fill"></i></button>
+              </div>
             </div>
           ))}
+          
         </div>
+
+}
+{
+    edit 
+    &&
+<div className="editSection">
+        <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder ="unit" value = {unit} onChange={(e)=> setUnit(e.target.value)} />
+        <input type="text" placeholder="referenceValue" value = {referenceValue}  onChange={(e)=> setReferenceValue(e.target.value)}/>
+        <button onClick={async ()=> {
+          setEdit(false)
+          const response = await axios.patch(`http://localhost:5000/service/${serviceId}/property/${parameterId}`,{
+            name,
+            unit,
+            referenceValue
+          })
+         if(response.status === 200) {
+          alert(`parameters changed successful`)
+          getServiceDetails(serviceId!)
+         } else {
+          alert("an error occured")
+         }
+          }}>SAVE</button>
       </div>
+
+      }
+      </div>
+
+      
     </div>
   );
 }
