f5f86a6f11fa5e543500f590e27591a8886fea74
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-07-26 16:58:37 +0100

    ....

--- lms_frontend/src/App.tsx
+++ lms_frontend/src/App.tsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 //import './App.css'
 //import "./index.css"
